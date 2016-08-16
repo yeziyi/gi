a6b5e4d5e0067f3174718cdfd03ec743eade86a6
“yijiajia1988 <yijiajia1988@gmail.com>
2016-08-16 17:37:58 +0800

    cccccc

---
+++ src/sagittarius/dev/googleip/DetectActivity.java
@@ -0,0 +1,18 @@
+package sagittarius.dev.googleip;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DetectActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+	}
+
+}
---
+++ src/sagittarius/dev/googleip/DetectServices3.java
@@ -0,0 +1,14 @@
+package sagittarius.dev.googleip;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class DetectServices3 extends Service {
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		return null;
+	}
+
+}
